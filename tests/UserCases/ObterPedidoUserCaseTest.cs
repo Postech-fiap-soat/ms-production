e6af977d48ad1e4ae635f2f4d6c08a7a74b7fb1e
Téury Simões Bazzo <teurybazzo@gmail.com>
2023-11-30 18:23:59 -0300

    add teste file para obter pedido user case

---
+++ tests/UserCases/ObterPedidoUserCaseTest.cs
@@ -0,0 +1,42 @@
+﻿using Aplication;
+using model;
+using Model.UserCases;
+using Moq;
+using Repositories;
+
+namespace Tests;
+
+public class ObterPedidoUserCaseTest
+{
+
+    [Fact]
+    public void DadoUmPedidoId_QuandoIdForDiferenteDeZero_DeveBuscarOPedido()
+    {
+        // Given
+        var pedido = new Pedido(1, Model.EStatusPedido.Recebido);
+
+        var pedidoRepositorio = new Mock<IPedidoRepository>();
+        pedidoRepositorio.Setup(x => x.ObterPedido(It.IsAny<int>())).Returns(pedido);
+
+        var usercase = new ObterPedidoUserCase(pedidoRepositorio.Object);
+
+        // When
+        var novoPedido = usercase.Handle(1);
+    
+        // Then
+        Assert.NotNull(novoPedido);
+    }
+
+    [Fact]
+    public void DadoUmPedidoId_QuandoIdForIgualZero_DeveDarExcption()
+    {
+        // Given
+        var pedidoRepositorio = new Mock<IPedidoRepository>();
+        var usercase = new ObterPedidoUserCase(pedidoRepositorio.Object);
+
+        // When
+        // Then
+        var ex = Assert.Throws<ArgumentException>(() => usercase.Handle(0));
+
+    }
+}
