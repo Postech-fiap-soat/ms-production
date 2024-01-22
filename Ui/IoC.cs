67c21b9eb087111344e33ebc624fb5d1e1bd4ab7
Téury Simões Bazzo <teurybazzo@gmail.com>
2024-01-22 20:28:19 -0300

    add nosonar para teste

--- Ui/IoC.cs
+++ Ui/IoC.cs
@@ -1,12 +1,13 @@
-﻿using Aplication;
+﻿using System.Diagnostics.CodeAnalysis;
+using Aplication;
 using Model.UserCases;
 using Repositories;
 
 namespace Ui;
 
+[ExcludeFromCodeCoverage]
 public static class Ioc
 {
-    //NOSONAR
     public static void AddIoc(this IServiceCollection serviceCollection){
         serviceCollection.AddScoped<IPedidoRepository, PedidoRepository>();
         serviceCollection.AddScoped<IAtualizarStatusPedidoUserCase, AtualizarStatusPedidoUserCase>();
